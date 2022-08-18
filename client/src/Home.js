7a96b36a665fd3b2841d155d0d6a11f359aa2e2b
Derek Vogt <derekvogt3@gmail.com>
2022-08-18 14:26:24 -0400

    installed tailwind

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
