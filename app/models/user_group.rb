38c8c21bd9a84827825c3e5d6e62f63d9135abfc
Derek Vogt <derekvogt3@gmail.com>
2022-08-24 09:30:58 -0400

    added validation

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -2,5 +2,8 @@ class UserGroup < ApplicationRecord
 
     belongs_to :user 
     belongs_to :group
+
+    
+  validates :user_id, uniqueness: {scope: :group_id}
     
 end
--- app/serializers/user_group_serializer.rb
+++ app/serializers/user_group_serializer.rb
@@ -1,3 +1,4 @@
 class UserGroupSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :group_id
+
 end
